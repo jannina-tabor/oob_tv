400e3e9293d17dadf2857c5dd84d7ed6e11a3484
Jannina <jaitabor2033@gmail.com>
2024-05-21 23:26:16 +0800

    Revised the Pseudocode; Made this into TV Class

--- oob_tv.py
+++ oob_tv.py
@@ -1,11 +1,19 @@
-# This program is about an application of OOB; Program #6
+# This program is about an application of OOB; Program #6: Class
 
 # Psuedocode
 
-# Copy the UML Class from the presentation
-# Name the UML Class "TV"
-# Create a separate python file for the Object
-# Name the Object file (TestDriver program) as TestTV
+# Based from the presentation, do a TV Class file
+# Set Class Attributes
+    # 'channel' to integer (by default as 1)
+    # 'volume' to integer (by default as 1)
+    # 'on' to boolean ( by default as false)
+# Initialize the attributes with the values
+# Implement 'turnOn() to set 'on' to True
+# Implement 'turnOff() to set 'on' to False
+# Implement 'getChannel() to return to the current 'channel'
+
+
+
 
 
 

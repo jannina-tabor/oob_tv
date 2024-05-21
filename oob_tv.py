ab08123a4d78ebd309ea31740d007a54332a9cf5
Jannina <jaitabor2033@gmail.com>
2024-05-21 23:33:44 +0800

    Finished the Pseudocode fot the TV Class

--- oob_tv.py
+++ oob_tv.py
@@ -1,16 +1,27 @@
-# This program is about an application of OOB; Program #6: Class
+# This program is about an application of OOB; Program #6: Class (TV)
 
 # Psuedocode
 
 # Based from the presentation, do a TV Class file
 # Set Class Attributes
     # 'channel' to integer (by default as 1)
-    # 'volume' to integer (by default as 1)
+    # 'volumeLevel' to integer (by default as 1)
     # 'on' to boolean ( by default as false)
 # Initialize the attributes with the values
 # Implement 'turnOn() to set 'on' to True
 # Implement 'turnOff() to set 'on' to False
 # Implement 'getChannel() to return to the current 'channel'
+# Implement 'setChannel (channel)' to set the 'channel' if the TV is on 
+    # Channel range should be within 1 to 120 
+# Implement 'getVolume()' to return to current 'volumeLevel'
+# Implement 'setVolume(volumeLevel)' to set the 'volumeLevel' if the TV is on
+    # Volume range should be within 1 to 7
+# Implement 'channelUp()' to increase the 'channel' by 1 
+# Implement 'channelDown()' to decrease the 'channel' by 1
+# Implement 'volumeUp()' to increase the 'volumeLevel' by 1
+# Implement 'volumeDown()' to decrease the 'volumeLevel' by 1
+
+
 
 
 

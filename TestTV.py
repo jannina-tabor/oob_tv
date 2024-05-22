6eb2280abba5ba28c049d32149dbada8efdefcd0
Jannina <jaitabor2033@gmail.com>
2024-05-22 19:20:03 +0800

    Created a Pseudocode for the TestTV Program

---
+++ TestTV.py
@@ -0,0 +1,14 @@
+# Program # 6; TestTv file
+
+# Pseudocode
+
+# Create the first TV Object labeled as 'tv1'
+# Turn on 'tv1'
+# Set 'tv1's channel to 30
+# Set 'tv1's volume to 3
+# Create the second TV Object labeled as 'tv2'
+# Turn on 'tv2'
+# Set 'tv1's channel to 3
+# Set 'tv1's volume to 2
+# Print 'tv1's channel and volume level
+# Print 'tv2's channel and volume level

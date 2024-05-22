9832f65d2e54dbed9a91209756b17897c601ad11
Jannina <jaitabor2033@gmail.com>
2024-05-22 20:15:25 +0800

    Changed the camelCase into snake_case in 'TestTV'

--- TestTV.py
+++ TestTV.py
@@ -18,16 +18,16 @@ from TV import TV
 
 def TestTV():
     tv1 = TV()
-    tv1.turnOn()
-    tv1.setChannel(30)
-    tv1.setVolume(3)
+    tv1.turn_on()
+    tv1.set_channel(30)
+    tv1.set_volume(3)
 
     tv2 = TV()
-    tv2.turnOn()
-    tv2.setChannel(3)
-    tv2.setVolume(2)
+    tv2.turn_on()
+    tv2.set_channel(3)
+    tv2.set_volume(2)
 
-    print(f"tv1's channel is {tv1.getChannel()} and volume level is {tv1.getVolume()}")
-    print(f"tv2's channel is {tv2.getChannel()} and volume level is {tv2.getVolume()}")
+    print(f"tv1's channel is {tv1.get_channel()} and volume level is {tv1.get_volume()}")
+    print(f"tv2's channel is {tv2.get_channel()} and volume level is {tv2.get_volume()}")
 
 TestTV()
